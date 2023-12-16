6cc0db3008d22b2648cdd79255a4d1282bd0d952
ShradhaSaxena23 <103318017+ShradhaSaxena23@users.noreply.github.com>
2023-12-16 14:56:58 +0530

    Update calender.js

--- calender.js
+++ calender.js
@@ -14,56 +14,33 @@ const optionsHindi = {
     timeZone: 'Asia/Kolkata', 
 };
 
+
 function showDate() {
+
+    document.getElementById("secondDate").value = "";
+ document.getElementById('secondinput').innerHTML = "";
+   document.getElementById('result').innerHTML = "";
+    document.getElementById("durationValue").value = "";
+    document.getElementById("durationType").value = "Days";
+  
+    
+    const radioButtons = document.getElementsByName("fav_language");
+    for (const radio of radioButtons) {
+      radio.checked = false;
+    }
+
   const x = document.getElementById("startDate").value;
   const formattedDate = new Intl.DateTimeFormat("hi-IN", options1).format(new Date(x));
-  document.getElementById('firstinput').innerHTML = formattedDate;
+  document.getElementById('firstinput').innerHTML = "Your desired start date is: <br>"+formattedDate;
 }
-
-function output() {
-  const startDate = new Date(document.getElementById("startDate").value);
-  const operation = document.getElementById("operation").value;
-  const durationValue = parseInt(document.getElementById("durationValue").value) || 0;
-  const durationType = document.getElementById("durationType").value;
-
-  let outputdate;
-
-  if (operation === "add") {
-      outputdate = new Date(startDate);
-      if (durationType === "days") {
-          outputdate.setDate(outputdate.getDate() + durationValue);
-      } else if (durationType === "months") {
-          outputdate.setMonth(outputdate.getMonth() + durationValue);
-      } else if (durationType === "weeks") {
-          outputdate.setDate(outputdate.getDate() + durationValue * 7);
-      } else if (durationType === "years") {
-          outputdate.setFullYear(outputdate.getFullYear() + durationValue);
-      }
-  } else if (operation === "subtract") {
-      outputdate = new Date(startDate);
-      if (durationType === "days") {
-          outputdate.setDate(outputdate.getDate() - durationValue);
-      } else if (durationType === "months") {
-          outputdate.setMonth(outputdate.getMonth() - durationValue);
-      } else if (durationType === "weeks") {
-          outputdate.setDate(outputdate.getDate() - durationValue * 7);
-      } else if (durationType === "years") {
-          outputdate.setFullYear(outputdate.getFullYear() - durationValue);
-      }
+function showDate2() {
+    const y = document.getElementById("secondDate").value;
+    const formattedDate = new Intl.DateTimeFormat("hi-IN", options1).format(new Date(y));
+    document.getElementById('secondinput').innerHTML = "Your desired second date is:<br> "+formattedDate;
   }
-  const resultElement = document.getElementById("result");
 
-  if (!isNaN(outputdate.getTime())) {
-    const formattedResult = new Intl.DateTimeFormat("hi-IN", optionsHindi).format(outputdate);
-    resultElement.innerHTML = ` ${formattedResult}`;
-} else {
-    resultElement.innerHTML = "Invalid date";
-}
 
-  return false;
-}
-
-const optionsHindi1 = {
+  const optionsHindi1 = {
     weekday: 'long',
     year: 'numeric',
     month: 'long',
@@ -79,3 +56,71 @@ document.getElementById('today').innerHTML ="Today : "+ formattedDate1;
 
 }
 window.onload = today;
+
+
+
+
+   
+            function output() {
+                const startDate = new Date(document.getElementById("startDate").value);
+                const secondDate = new Date(document.getElementById("secondDate").value);
+                const operationType = document.querySelector('input[name="fav_language"]:checked');
+                const durationValue = parseInt(document.getElementById("durationValue").value) || 0;
+                const durationType = document.getElementById("durationType").value;
+    
+                let resultMessage;
+    
+                if (secondDate.toString() !== "Invalid Date") {
+                  
+                    const timeDifference = Math.abs(secondDate - startDate);
+                    const differenceInDays = Math.floor(timeDifference / (1000 * 3600 * 24));
+                    const differenceInMonths = (secondDate.getFullYear() - startDate.getFullYear()) * 12 + (secondDate.getMonth() - startDate.getMonth());
+                    const differenceInYears = secondDate.getFullYear() - startDate.getFullYear();
+                    resultMessage = `Difference: ${differenceInDays} days, ${differenceInMonths} months, ${differenceInYears} years`;
+                } else if (operationType && durationValue > 0 && durationType) {
+                    
+                    const outputDate = new Date(startDate);
+    
+                    if (operationType.value === "Add") {
+                        if (durationType === "days") {
+                            outputDate.setDate(outputDate.getDate() + durationValue);
+                        } else if (durationType === "weeks") {
+                            outputDate.setDate(outputDate.getDate() + durationValue * 7);
+                        } else if (durationType === "months") {
+                            outputDate.setMonth(outputDate.getMonth() + durationValue);
+                        } else if (durationType === "years") {
+                           
+                            outputDate.setFullYear(outputDate.getFullYear() + durationValue);
+
+                        }
+                    }
+                    else if (operationType.value === "Subtract") {
+                        if (durationType === "days") {
+                            outputDate.setDate(outputDate.getDate() - durationValue);
+                        } else if (durationType === "months") {
+                            outputDate.setMonth(outputDate.getMonth() - durationValue);
+                        } else if (durationType === "weeks") {
+                            outputDate.setDate(outputDate.getDate() - durationValue * 7);
+                        }
+                        else if (durationType === "years") {
+                           
+                            outputDate.setFullYear(outputDate.getFullYear() - durationValue);
+
+                        }
+                    }
+                    const formattedResult = new Intl.DateTimeFormat("hi-IN", optionsHindi).format(outputDate);
+                resultMessage = `Result Date: ${formattedResult}`;
+            } else {
+                resultMessage = "Invalid input. Please provide all necessary information.";
+            }
+
+            document.getElementById("result").innerHTML = resultMessage;
+    
+    
+    
+    
+    
+
+  
+    return false;
+  }
